3a8a7603c5e1e39603c07512392df2a453750c06
Edgar <edgar.nurmagomedov@mail.ru>
2023-08-28 22:04:29 +0500

    Test

--- src/test/java/settings/SeleniumConfiguration.java
+++ src/test/java/settings/SeleniumConfiguration.java
@@ -160,15 +160,14 @@ public abstract class SeleniumConfiguration {
             Allure.step("Language: " + languages + " Countries: " + countries);
             Actions actions = new Actions(getDriver());
             actions.moveToElement(educatedMove.getHdrIcon())
-                    .pause(Duration.ofSeconds(1))
                     .click(educatedMove.getDropDownCountry())
-                    .pause(Duration.ofSeconds(1))
                     .perform();
             actions.moveToElement(educatedMove.getCountryList())
                     .scrollToElement(educatedMove.getCountryDe())
-                    .pause(Duration.ofSeconds(1))
-                    .click(educatedMove.getCountryDe())
+                   // .click(educatedMove.getCountryDe())
                     .perform();
+            educatedMove.fluentWaitLocators(educatedMove.getCountryDe());
+            educatedMove.getCountryDe();
         }
     }
 
